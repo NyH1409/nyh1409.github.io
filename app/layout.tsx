a53fc51f4aad6920adb84c876e6aff96f688dbaa
Ny Hasina Vagno <hasina.vagno@esiroi.re>
2026-02-03 23:11:21 +0400

    feat: add google tag manager conf

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/provider/theme-provider";
+import Script from "next/script"; // 1. On importe le composant Script
 
 export const metadata: Metadata = {
   title: "Ny Hasina VAGNO",
@@ -15,8 +16,30 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <link rel="icon" href="photo.jpeg" type="image/*" />
+      <head>
+        <link rel="icon" href="photo.jpeg" type="image/*" />
+        {/* 2. On utilise Script avec une stratégie optimisée */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-T7ZLX6T7');
+          `}
+        </Script>
+      </head>
       <body>
+        {/* 3. Ajout du code noscript (obligatoire pour le fonctionnement complet) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T7ZLX6T7"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
